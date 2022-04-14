5ff0d6a247d21d028c1a96a0175cdfc96befdb69
rossen1344 <95756425+rossen1344@users.noreply.github.com>
2022-04-14 17:06:44 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const text = document.getElementById("text");
+const addTaskButton = document.getElementById("add-task-btn");
+const saveTaskButton = document.getElementById("save-todo-btn");
+const listBox = document.getElementById("listBox");
+const saveInd = document.getElementById("saveIndex");
+
+let todoArray = [];
+
