c63f6cdacfda9bd735adebe26fa7bca3b8568041
rossen1344 <95756425+rossen1344@users.noreply.github.com>
2022-04-16 16:47:36 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,3 +6,35 @@ const saveInd = document.getElementById("saveIndex");
 
 let todoArray = [];
 
+addTaskButton.addEventListener("click", (a) => {
+a.preventDefault();
+let todo = localStorage.getItem("todo");
+
+if (todo === null) {
+    todoArray = [];
+} else {
+    todoArray = JSON.parse(todo);
+}  
+todoArray.push(text.value);
+text.value = "";
+localStorage.setItem("todo", JSON.stringify(todoArray));
+ displayTodo();
+});
+
+function displayTodo() {
+    let todo = localStorage.getItem("todo");
+    if (todo === null) {
+        todoArray = [];
+    } else {
+        todoArray = JSON.parse(todo);
+    }
+    let htmlCode = "";
+    todoArray.forEach((list, index) => {
+        htmlCode += `<div>
+            <p>${list}</p>
+            <button onclick='edit(${index})'>Edit</button>
+           <button onclick='deleteTodo(${ind})'>Delete</button>
+        </div>`;
+    });
+    listBox.innerHTML = htmlCode;
+}
