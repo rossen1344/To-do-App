81920e57c3a283ec4fc2dca5e26f47077272c016
rossen1344 <95756425+rossen1344@users.noreply.github.com>
2022-04-19 15:18:49 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const text = document.getElementById("text");
-const addTaskButton = document.getElementById("add-task-btn");
-const saveTaskButton = document.getElementById("save-todo-btn");
-const listBox = document.getElementById("listBox");
-const saveInd = document.getElementById("saveIndex");
+const text = document.getElementById('text');
+const addTaskButton = document.getElementById('add-task-btn');
+const saveTaskButton = document.getElementById('save-todo-btn');
+const listBox = document.getElementById('listBox');
+const saveIndex = document.getElementById('saveIndex');
 
 let todoArray = [];
 
-addTaskButton.addEventListener("click", (a) => {
+addTaskButton.addEventListener('click', (a) => {
 a.preventDefault();
-let todo = localStorage.getItem("todo");
+let todo = localStorage.getItem('todo');
 
 if (todo === null) {
     todoArray = [];
@@ -16,25 +16,54 @@ if (todo === null) {
     todoArray = JSON.parse(todo);
 }  
 todoArray.push(text.value);
-text.value = "";
-localStorage.setItem("todo", JSON.stringify(todoArray));
+text.value = '';
+localStorage.setItem('todo', JSON.stringify(todoArray));
  displayTodo();
 });
 
 function displayTodo() {
-    let todo = localStorage.getItem("todo");
+    let todo = localStorage.getItem('todo');
     if (todo === null) {
         todoArray = [];
     } else {
         todoArray = JSON.parse(todo);
     }
-    let htmlCode = "";
+    let htmlCode = '';
     todoArray.forEach((list, index) => {
         htmlCode += `<div>
             <p>${list}</p>
             <button onclick='edit(${index})'>Edit</button>
-           <button onclick='deleteTodo(${ind})'>Delete</button>
+            <button onclick='deleteTodo(${index})'>Delete</button>
         </div>`;
     });
     listBox.innerHTML = htmlCode;
 }
+
+function deleteTodo(index) {
+    let todo = localStorage.getItem('todo');
+    todoArray = JSON.parse(todo);
+    todoArray.splice(index, 1);
+    localStorage.setItem('todo', JSON.stringify(todoArray));
+    displayTodo();
+}
+
+function edit(index) {
+    saveIndex.value = index;
+    let todo = localStorage.getItem('todo');
+    todoArray = JSON.parse(todo);
+    text.value = todoArray[index];
+    addTaskButton.style.display = "none";
+    saveTaskButton.style.display = "block";
+}
+
+saveTaskButton.addEventListener('click', () => {
+    let todo = localStorage.getItem('todo');
+    todoArray = JSON.parse(todo);
+    let id = saveIndex.value;
+    todoArray[id] = text.value;
+    addTaskButton.style.display = 'block';
+    saveTaskButton.style.display = 'none';
+    text.value = '';
+    localStorage.setItem('todo', JSON.stringify(todoArray));
+    displayTodo();
+   });
